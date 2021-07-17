96a5921c73d547c2c56640d78c3def399f2f6fb4
yobright <mbahiag@gmail.com>
2021-07-17 23:10:41 +0200

    Hulu Clone APP

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+import Nav from '../components/Nav'
+import Results from '../components/Results'
+import requests from '../utils/requests'
+
+export default function Home({ results }) {
+ 
+  return (
+    <div>
+      <Head>
+          <title>VVVS</title>
+          <link rel="icon" href='/favicon.ico'/> 
+      </Head>
+      <Header /> 
+      <Nav /> 
+      
+      <Results results={results}/>
+    </div>
+  )
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre; 
+  const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || request.fetchTrending.url}`).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results
+    }
+  }
+}
---
+++ utils/requests.js
@@ -0,0 +1,50 @@
+const API_KEY = process.env.API_KEY 
+
+export default {
+    fetchTrending: {
+        title: 'Trending',
+        url:`/trending/all/week?api_key=${API_KEY}&language=fr` 
+    },
+    fetchTopRated: {
+        title: 'Top Rated',
+        url:`/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1` 
+    },
+    fetchActionMovies: {
+        title: 'Action',
+        url:`/discover/movie?api_key=${API_KEY}&with_genres=28` 
+    },
+    fetchComedyMovies: {
+        title: 'Comedy',
+        url:`/discover/movie?api_key=${API_KEY}&with_genres=35` 
+    },
+    fetchHorrorMovies: {
+        title: 'Horror',
+        url:`/discover/movie?api_key=${API_KEY}&with_genres=27` 
+    },
+    fetchMystery: {
+        title: 'Mystery',
+        url:`/discover/movie?api_key=${API_KEY}&with_genres=9648` 
+    },
+    fetchRomancesMovies: {
+        title: 'Romance',
+        url:`/discover/movie?api_key=${API_KEY}&with_genres=10749` 
+    },
+    fetchSciFi: {
+        title: 'Sci-Fi',
+        url:`/discover/movie?api_key=${API_KEY}&with_genres=878` 
+    }, 
+    fetchWestern: {
+        title: 'Western',
+        url:`/discover/movie?api_key=${API_KEY}&with_genres=37` 
+    },
+    fetchAnimation: {
+        title: 'Animation',
+        url:`/discover/movie?api_key=${API_KEY}&with_genres=16` 
+    }, 
+    fetchTV: {
+        title: 'TV Movie',
+        url:`/trending/all/week?api_key=${API_KEY}&language=fr` 
+    }
+
+
+}
